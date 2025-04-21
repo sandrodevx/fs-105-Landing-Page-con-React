ae2f67b789ed162cc0d723313f03faa9ce7a0e72
Sandro Gomez <sandrodevx@gmail.com>
2025-04-21 19:20:09 +0000

    Landing Page

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
-import {
-    defineConfig
-} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000
-    },
-    build: {
-        outDir: 'dist',
-    }
-})
+  plugins: [react()],
+  server: {
+    port: 3000, 
+    open: true 
+  }
+});
